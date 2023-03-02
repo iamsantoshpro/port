e1a4346c8b5da7b580f5ca96e1e08939c178038f
Santosh Kumar <46213433+iamsantoshpro@users.noreply.github.com>
2023-03-02 21:58:06 +0530

    new

--- src/Component/Hello.js
+++ src/Component/Hello.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import DownloadIcon from '../Img/img_btn_icon.png'
 import ReactTypingEffect from 'react-typing-effect';
-import cv from '../updated_CV.pdf'
+import cv from '../updated_march_cv.pdf'
 
 
 export default function Hello() {
